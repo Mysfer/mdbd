1b6b6cc300df73fae8cac253a28d05ca8ae7e0ed
07-13 <07-13>
2017-10-30 15:18:03 -0200

    nav TOP !!!!!

--- dicionario/app/views/index.php
+++ dicionario/app/views/index.php
@@ -6,24 +6,28 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="/public/css/foundation.css">
     </head>
-    <body>    
-        <nav class="foundation-bar h3">
+    <body>
+        <div class="menu submenu-toggle foundation-bar h3">   
+            <nav>
+                <ul>
+                    <li><a href="/">Index</a></li>
+                    <li><a href="/cadastro/palavra">Cadastrar Palavra</a></li>                    
+                    <li><a href="/cadastro/dicionario">Cadastrar Dicionario</a></li>
+                    <li><a href="/link/sinonimo">Linkar Sinonimo</a></li>
+                    <li><a href="/link/dicionario">Linkar Dicionario</a></a></li>
+                    <li><a href="/faleconosco">Fale conosco</a></li>
+                    
+                </ul>
+            </nav>
+        </div>
+        <div class="h5">
             <ul>
-                <li><a href="/">Index</a></li>
-                <li><a href="/cadastro/palavra">Cadastrar Palavra</a></li>
-                <li><a href="/cadastro/palavra">Cadastrar Palavra</a></li>
-                <li><a href="/cadastro/dicionario">Cadastrar Dicionario</a></li>
-                <li><a href="/link/sinonimo">Linkar Sinonimo</a></li>
-                <li><a href="/link/dicionario">Linkar Dicionario</a></a></li>
-                <li><a href="/faleconosco">Fale conosco</a></li>
-                
-            </ul>
-        </nav>
-            <?php foreach($dados as $d) { ?>
-                
-                <li><a href="/consultar?id=<?= $d['id'] ?>"><?= $d['palavra'] ?></a></li>
+                <?php foreach($dados as $d) { ?>
+                    
+                    <li><a href="/consultar?id=<?= $d['id'] ?>"><?= $d['palavra'] ?></a></li>
 
-            <?php  } ?>
-        </ul>
+                <?php  } ?>
+            </ul>
+        </div>
     </body>
 </html>
