5f77e70e62b94069a1d8a2f40b455baf044883e9
10-13 <10-13>
2017-11-08 15:39:44 -0200

    grid

--- dicionario/app/views/index.php
+++ dicionario/app/views/index.php
@@ -11,27 +11,33 @@
         <header>
             <h1>Dicionline Emira</h1>
         </header>
-        <section class='grid-x grid-padding-x lateral-section'>   
-            <nav class="large-6 medium-6 cell h3">
-                <ul>
-                    <li><a href="/">Index</a></li>
-                    <li><a href="/cadastro/palavra">Cadastrar Palavra</a></li>                    
-                    <li><a href="/cadastro/dicionario">Cadastrar Dicionario</a></li>
-                    <li><a href="/link/sinonimo">Linkar Sinonimo</a></li>
-                    <li><a href="/link/dicionario">Linkar Dicionario</a></a></li>
-                    <li><a href="/faleconosco">Fale conosco</a></li>
-                </ul>
-            </nav>
-        </section>
-        <div class="h5">
-            <ul>
-                <?php foreach($dados as $d) { ?>
-                    
-                    <li><a href="/consultar?id=<?= $d['id'] ?>"><?= $d['palavra'] ?></a></li>
+        <section class='grid-x grid-padding-x'>
 
-                <?php  } ?>
-            </ul>
-        </div>
+            <div class="large-8 medium-8 cell">
+                <h2>Sugestões:</h2>
+                <ul class="h5">
+                    <?php foreach($dados as $d) { ?>
+                        
+                        <li><a href="/consultar?id=<?= $d['id'] ?>"><?= $d['palavra'] ?></a></li>
+
+                    <?php  } ?>
+                </ul>
+            </div>
+
+            <div class="large-2 medium-2 cell h3">
+                <nav>
+                    <ul>
+                        <li><a href="/">Index</a></li>
+                        <li><a href="/cadastro/palavra">Cadastrar Palavra</a></li>                    
+                        <li><a href="/cadastro/dicionario">Cadastrar Dicionario</a></li>
+                        <li><a href="/link/sinonimo">Linkar Sinonimo</a></li>
+                        <li><a href="/link/dicionario">Linkar Dicionario</a></a></li>
+                        <li><a href="/faleconosco">Fale conosco</a></li>
+                    </ul>
+                </nav>
+            </div>
+
+        </section>
 
         <script src="./public/js/vendor/jquery.js"></script>
         <script src="./public/js/vendor/what-input.js"></script>
